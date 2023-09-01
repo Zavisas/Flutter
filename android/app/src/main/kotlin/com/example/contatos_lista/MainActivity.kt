f0830ecb1904bbf90165f86f67f48c7e39dee82a
Joao Zavisas <joaoterassi01@gmail.com>
2023-09-01 10:01:23 -0300

    Primeiro projeto flutter

---
+++ android/app/src/main/kotlin/com/example/contatos_lista/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contatos_lista
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Contact {
+  final String name; //nome do aluno
+  final String RA; // RA do aluno
+  final String imagePath; // Imagem
+
+  Contact(this.name, this.RA, this.imagePath);
+}
+
+class MyApp extends StatelessWidget {
+  final List<Contact> contacts = [
+    Contact('João Zavisas', 'RA: 1431432312032', 'assets/contact_images/yudi.png'),
+    Contact('Ana Flavia', 'RA: 000000000000', 'assets/contact_images/woman.png'),
+    Contact('Flavia Silva', 'RA: 1111111111111', 'assets/contact_images/woman2.png'),
+    Contact('Machado De Assis', 'RA: 2222222222222', 'assets/contact_images/homem.png')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green, //cor container
+          title: Text('Lista de Contatos | Fatec '), //texto dentro desse container
+        ),
+        body: ContactList(contacts),
+      ),
+    );
+  }
+}
+
+class ContactList extends StatelessWidget {
+  final List<Contact> contacts;
+
+  ContactList(this.contacts);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: contacts.length,
+      itemBuilder: (ctx, index) {
+        return ListTile(
+          leading: Container(
+            width: 50,
+            height: 50,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: ClipOval(
+              child: Image.asset(
+                contacts[index].imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          title: Text(contacts[index].name),
+          subtitle: Text(contacts[index].RA),
+        );
+      },
+    );
+  }
+}
